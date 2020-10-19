341802524b64c9f29412ce82d56d4e0ff175868a
Garman_K <garnetbirthstone@hotmail.com>
2020-10-19 19:44:44 -0400

    Created questions array and added require files

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,43 @@
+const fs = require( 'fs' )
+const inquirer = require( 'inquirer' )
+
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title of project:'
+    },
+    {
+        type: 'input',
+        name: 'desc',
+        message: 'Project description:'
+    },
+    {
+        type: 'input',
+        name: 'inst',
+        message: 'Installation notes:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage notes:'
+    },
+    {
+        type: 'number',
+        name: 'license',
+        message: 'Choose your License:'
+    },
+    {
+        type: 'input',
+        name: 'contr',
+        message: 'Enter contributing members separated by commas:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Test notes:'
+    }
 
 ];
 
@@ -14,3 +52,13 @@ function init() {
 
 // function call to initialize program
 init();
+
+// Title
+// Description
+// Table of Contents
+// Installation
+// Usage
+// License
+// Contributing
+// Tests
+// Questions
