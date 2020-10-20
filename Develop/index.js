61eefd5d84c23d7e776b56f64b6d3077eb5b7a89
Garman_K <garnetbirthstone@hotmail.com>
2020-10-20 01:10:15 -0400

    Console.log prelim display

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 const fs = require( 'fs' )
-const inquirer = require( 'inquirer' )
+const inquirer = require( 'inquirer' );
+const Choices = require('inquirer/lib/objects/choices');
 
 // array of questions for user
 const questions = [
@@ -24,29 +25,60 @@ const questions = [
         message: 'Usage notes:'
     },
     {
-        type: 'number',
+        type: 'list',
         name: 'license',
-        message: 'Choose your License:'
+        message: 'Choose your License:',
+        choices: ['MIT','GNU GPL v3']
     },
     {
         type: 'input',
         name: 'contr',
-        message: 'Enter contributing members separated by commas:'
+        message: 'Enter contributing members:'
     },
     {
         type: 'input',
         name: 'tests',
         message: 'Test notes:'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'GitHub username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Your email:'
     }
 
 ];
-
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+//     fs.writeFileSync( 'CONTRIBUTORS.md',
+//     `------------------------------------------
+// #${questions.title}
+
+// ${questions.desc}` )
+// }
 
 // function to initialize program
-function init() {
+async function init() {
+    const setup = await inquirer.prompt(questions);
+    if (setup.license == 'MIT'){
+        const pick = 'MIT License';
+    }
+    else{
+        const pick = 'GNU GPL v3';
+    }
+    console.log(
+        `#${setup.title}\n${setup.desc}\n`+
+        `##Installation\n${setup.inst}\n`+
+        `##Usage\n${setup.usage}\n`+
+        `##License\n${setup.license}\n`+
+        `##Contributing\n${setup.contr}\n`+
+        `##Tests\n${setup.tests}\n`+
+        `##Questions\n<https://github.com/${setup.github}>\nReach me with additional questions at: <${setup.email}>`
+    );
 
 }
 
